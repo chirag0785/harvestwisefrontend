d2a5b3ea70cf01ea37454fe413c1f6f9377fc767
Chirag Bhatia <chiragbhatia.here2005@gmail.com>
2024-07-27 22:41:14 +0530

    frontend

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    rules: {
+        'no-unused-vars': 'warn',
+        'react-hooks/exhaustive-deps': 'warn',
+        // Add more rules here as needed, set them to 'warn'
+    },
+};
