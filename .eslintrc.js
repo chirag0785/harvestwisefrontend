60b87b0e3b982163560c0c83cd4783ced928350c
Chirag Bhatia <chiragbhatia.here2005@gmail.com>
2024-07-27 22:48:34 +0530

    frontend

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,14 @@
 module.exports = {
+    "extends": [
+        "react-app",
+        "react-app/jest"
+    ],
     rules: {
         'no-unused-vars': 'warn',
         'react-hooks/exhaustive-deps': 'warn',
         // Add more rules here as needed, set them to 'warn'
     },
+    "plugins": [
+        "react-hooks" // Ensure this plugin is included
+    ]
 };
